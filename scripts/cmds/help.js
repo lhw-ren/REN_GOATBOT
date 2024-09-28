e7dc21fc59323cef66d80d6e2891197c8e6c5c8a
lhw-ren <yeshuasynovic@gmail.com>
2024-09-28 21:49:16 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -7,6 +7,6 @@ module.exports = {
         guide: "{pn}"
     },
     onStart: async function ({ message }) {
-        message.reply("Hi, I'm RTUMM AI, your AI buddy created by Math Major. Just type my prefix (#) to ask me anything.\n🎀————————————🎀\nLIST OF BOT'S COMMANDS\n🎀————————————🎀\n#help [command name]\n#math [calculate]\n#calculate [provide an expression to calculate]\n#ai [query]\n#gemini [query]\n#define [word]\n#dictionary [lookup at dictionary]\n#quiz [subject]\n#guessnumber [number game]\n#google [query]\n#imgsearch [search image]\n#owner [bot owner]\n🎀—————————🎀\ntype #<command name> prompt, and wait to the results.🌹");
+        message.reply("Hi! I'm RTUMM AI, your AI buddy created by Math Major. Just type my prefix (#) to ask me anything.\n🎀————————————🎀\nLIST OF BOT'S COMMANDS\n🎀————————————🎀\n#help [command name]\n#math [calculate]\n#calculate [provide an expression to calculate]\n#ai [query]\n#gemini [query]\n#define [word]\n#dictionary [lookup at dictionary]\n#quiz [subject]\n#guessnumber [number game]\n#google [query]\n#imgsearch [search image]\n#owner [bot owner]\n🎀—————————🎀\ntype #<command name> prompt, and wait to the results.🌹");
     }
 };
